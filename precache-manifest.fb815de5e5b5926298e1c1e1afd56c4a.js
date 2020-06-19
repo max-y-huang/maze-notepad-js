1e6977e0cb26e0c832c20bf5c50738b3e964078c
Max Huang <letmaxknow@gmail.com>
2020-06-19 16:45:56 -0400

    Updates

---
+++ precache-manifest.fb815de5e5b5926298e1c1e1afd56c4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c386743b80bb1e133d3a327d28835f72",
+    "url": "/maze-notepad-js/index.html"
+  },
+  {
+    "revision": "31000b4e0d280faa8732",
+    "url": "/maze-notepad-js/static/css/main.6a9bacff.chunk.css"
+  },
+  {
+    "revision": "f1fdf62b1a1337edf80b",
+    "url": "/maze-notepad-js/static/js/2.b46fe474.chunk.js"
+  },
+  {
+    "revision": "ebc14457535110648808604cc4b96810",
+    "url": "/maze-notepad-js/static/js/2.b46fe474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31000b4e0d280faa8732",
+    "url": "/maze-notepad-js/static/js/main.d9710dce.chunk.js"
+  },
+  {
+    "revision": "757c8a6acb7098b5b24d",
+    "url": "/maze-notepad-js/static/js/runtime-main.f0280433.js"
+  }
+]);
