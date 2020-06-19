a80b509db4967644343b1b810674dda3740675dc
Max Huang <letmaxknow@gmail.com>
2020-06-19 17:25:08 -0400

    Updates

--- precache-manifest.48d2720d31bdefd390828a2d60b847a3.js
+++ precache-manifest.48d2720d31bdefd390828a2d60b847a3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a4114bacc4af04f5003520d3314b203f",
+    "revision": "ed5823dbcd27e0d7edf1a382bb77c25a",
     "url": "/maze-notepad-js/index.html"
   },
   {
-    "revision": "87e8b87dc44a486ec02c",
+    "revision": "9f3cee936613ff07da51",
     "url": "/maze-notepad-js/static/css/main.8c9fe9fc.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/maze-notepad-js/static/js/2.b46fe474.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "87e8b87dc44a486ec02c",
-    "url": "/maze-notepad-js/static/js/main.04893580.chunk.js"
+    "revision": "9f3cee936613ff07da51",
+    "url": "/maze-notepad-js/static/js/main.4e2f4dd6.chunk.js"
   },
   {
     "revision": "757c8a6acb7098b5b24d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/maze-notepad-js/precache-manifest.b2b5f4464a49fc5937388120d1d66f88.js"
+  "/maze-notepad-js/precache-manifest.48d2720d31bdefd390828a2d60b847a3.js"
 );
 
 self.addEventListener('message', (event) => {
