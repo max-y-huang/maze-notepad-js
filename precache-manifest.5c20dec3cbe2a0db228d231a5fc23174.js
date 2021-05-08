8463dae7993c70904f8729c2c1c12393a19e9715
Max Huang <letmaxknow@gmail.com>
2021-05-08 13:39:32 -0400

    Updates

---
+++ precache-manifest.5c20dec3cbe2a0db228d231a5fc23174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4a7fd81a6d1ee2851c70bf242b9ca8",
+    "url": "/maze-notepad-js/index.html"
+  },
+  {
+    "revision": "f8656cd713f11657f639",
+    "url": "/maze-notepad-js/static/css/main.e84898ec.chunk.css"
+  },
+  {
+    "revision": "11b16b30d3b3f137be05",
+    "url": "/maze-notepad-js/static/js/2.4f3f5ce0.chunk.js"
+  },
+  {
+    "revision": "1991551b7858c0979a0db5af1a0a56af",
+    "url": "/maze-notepad-js/static/js/2.4f3f5ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8656cd713f11657f639",
+    "url": "/maze-notepad-js/static/js/main.4b650908.chunk.js"
+  },
+  {
+    "revision": "757c8a6acb7098b5b24d",
+    "url": "/maze-notepad-js/static/js/runtime-main.f0280433.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/maze-notepad-js/precache-manifest.b313631c9c4edd9022f38f423cadd714.js"
+  "/maze-notepad-js/precache-manifest.5c20dec3cbe2a0db228d231a5fc23174.js"
 );
 
 self.addEventListener('message', (event) => {
